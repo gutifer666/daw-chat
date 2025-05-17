8ad32fc493d2ee8f86580541d24ae6516eef329b
Javier <gutifer666@gmail.com>
2025-05-17 23:40:37 +0200

    added Newsletter component

---
+++ js/newsletter-component.js
@@ -0,0 +1,39 @@
+// Definición de la clase del elemento personalizado
+class NewsletterComponent extends HTMLElement {
+    constructor() {
+        super(); // Llamamos al constructor de HTMLElement
+    }
+
+    connectedCallback() {
+        // Leer atributos personalizados (si no existen, usar valores por defecto)
+        const titulo = this.getAttribute('titulo') || 'Para saber más';
+        const descripcion = this.getAttribute('descripcion') || 
+            'No te pierdas las nuevas unidades de trabajo que se van añadiendo contínuamente a la plataforma, así como todas las actualizaciones de contenido. Suscríbete al Newsletter y mantente contínuamente informado. Puedes darte de baja en cualquier momento.';
+        const textoBoton = this.getAttribute('texto-boton') || 'Seguir Actualizaciones';
+        const enlace = this.getAttribute('enlace') || 'https://www.getrevue.co/profile/javigutiblogger';
+
+        // Construir el contenido HTML interno del componente
+        this.innerHTML = `
+        <div class="iDevice_wrapper ParasabermasfpdIdevice em_iDevice" id="id2">
+          <div class="iDevice emphasis_parasabermasfpd">
+            <div class="iDevice_header">
+              <h2 class="iDeviceTitle">${titulo}</h2>
+            </div>
+            <div class="iDevice_inner">
+              <div class="iDevice_content_wrapper">
+                <div class="block iDevice_content">
+                  <p>${descripcion}</p>
+                  <p class="enlace_centrado">
+                    <a href="${enlace}" title="Acceso al Newsletter" target="_blank">${textoBoton}</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        `;
+    }
+}
+
+// Registrar el nuevo elemento personalizado en el navegador
+customElements.define('newsletter-component', NewsletterComponent);
